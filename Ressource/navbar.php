c891db9251060428cf5d2867fcb58821186d58c1
David <david.roquain@edu.esiee-it.fr>
2022-02-09 15:01:28 +0100

    Debut

---
+++ Ressource/navbar.php
@@ -0,0 +1,6 @@
+<nav>
+    <button>Catégorie</button>
+    <button>Profil</button>
+    <button>SignUp</button>
+    <button>Login</button>
+</nav>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GameCon Seil</title>
+        <meta name="description" content="">
+    </head>
+    <body>
+        <?php
+        include_once("Ressource/navbar.php");
+        ?>
+    </body>
+</html>
