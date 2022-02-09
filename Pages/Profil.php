9189ce0bb06e02a5470fd23d8a297b5d303d57fc
David <david.roquain@edu.esiee-it.fr>
2022-02-09 15:56:26 +0100

    Vol de contenu chez Antoine

--- Pages/Profil.php
+++
@@ -1,5 +0,0 @@
-<form>
-    <h2>Profil</h2>
-        <p>$Pseudo</p>
-        <button method="get" name="modif" value="true">Modifier</button>
-</form>
--- Ressource/categorie.php
+++
@@ -1,5 +0,0 @@
-<nav>
-    <button >Nintendo Switch</button>
-    <button>Playstation 5</button>
-    <button>Xbox Series X</button>
-</nav>
