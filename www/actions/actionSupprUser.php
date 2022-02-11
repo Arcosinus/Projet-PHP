6629f0862e0701e32ee496e32bcaefee817949ac
David <david.roquain@edu.esiee-it.fr>
2022-02-11 09:03:53 +0100

    Résolution énigme parenthèse

--- www/actions/actionSupprUser.php
+++ www/actions/actionSupprUser.php
@@ -1,18 +1,14 @@
 <?php
 require_once '../../src/db.php';
-?>
+if (!(empty($_GET['IdSupr'])))
+{
 
-<?php
-if (!(empty($_GET['IdSupr'])){
-
-    $email = $_GET['IdSupr'];
-
-    $sql ='DELETE FROM user WHERE idUser as :idUser';
+    $sql = 'DELETE FROM user WHERE idUser as :idUser';
 
     $querry = $db->prepare($sql);
 
     $querry->execute([
-    ':idUser'=> $id,
+    ':idUser'=> $_GET['IdSupr'],
     ]);
 
     $user = $querry->fetch(PDO::FETCH_ASSOC);
